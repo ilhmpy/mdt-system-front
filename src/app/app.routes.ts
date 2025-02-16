b2dbeb7cac252f1313b4fcd0a70e367b86a81088
ihlmpy <jsih1236@gmail.com>
2025-02-16 17:02:39 +0100

    Initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { OfficersComponent } from './officers/officers.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { pathMatch: "full", path: "", redirectTo: "officers" },
+    { path: "officers", component: OfficersComponent }
+];
